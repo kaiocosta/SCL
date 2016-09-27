a1fb2194cff6b8562c2c20894a56f10deb4829d2
Kaio <Kaio@192.168.0.11>
2016-09-27 09:43:46 -0300

    commit 2/09
    Servlet consulta cliente pronto

--- src/java/modelo/Cliente.java
+++ src/java/modelo/Cliente.java
@@ -6,17 +6,30 @@
 
 package modelo;
 
+import dao.ClienteDAO;
+import dao.FilmesDAO;
 import java.sql.Array;
+import java.sql.SQLException;
 import java.util.ArrayList;
+import java.util.List;
 
 /**
  *
  * @author Aluno
  */
 public class Cliente extends Pessoa {
+
+    public static List<Cliente> obterClientes() 
+        throws ClassNotFoundException, SQLException {
+        
+        return ClienteDAO.obterClientes();
+    
+    } 
+    
   
-    private ArrayList dependentes = new ArrayList();
+    private ArrayList dependentes;
     private int id_cliente;
+    private static ArrayList<Locadora> historico;
 
     public Cliente(String nome, String sexo, String dataDeNasc, String estadoCivil, String cpf, String rg, int id_cliente) {
         this.setNome(nome);
@@ -26,6 +39,15 @@ public class Cliente extends Pessoa {
         this.setCpf(cpf);
         this.setRg(rg);
         this.id_cliente = id_cliente;
+        new ArrayList<Locadora>();
+    }
+
+    public static ArrayList<Locadora> getHistorico() {
+        return historico;
+    }
+
+    public static void setHistorico(ArrayList<Locadora> historico) {
+        Cliente.historico = historico;
     }
 
     public ArrayList getDependentes() {
@@ -33,6 +55,6 @@ public class Cliente extends Pessoa {
     }
 
     public void setDependentes(ArrayList dependentes) {
-        this.dependentes = dependentes;
+        this.dependentes.add(dependentes);
     }
 }
