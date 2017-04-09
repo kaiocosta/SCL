f00923683a71678d222f68d557e39fd515c238d9
Ramon de Souza Almeida <ramonsouza.92@hotmail.com>
2017-04-09 15:43:59 -0300

    banco com alguns itens

--- sclbd.sql
+++ sclbd.sql
@@ -1,11 +1,11 @@
 -- phpMyAdmin SQL Dump
--- version 4.5.5.1
--- http://www.phpmyadmin.net
+-- version 4.6.4
+-- https://www.phpmyadmin.net/
 --
 -- Host: 127.0.0.1
--- Generation Time: 29-Mar-2017 às 20:43
--- Versão do servidor: 5.7.11
--- PHP Version: 7.0.4
+-- Generation Time: 09-Abr-2017 às 18:43
+-- Versão do servidor: 5.7.14
+-- PHP Version: 5.6.25
 
 SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
 SET time_zone = "+00:00";
@@ -39,8 +39,10 @@ CREATE TABLE `categorias` (
 --
 
 INSERT INTO `categorias` (`codCategoria`, `nome`, `descricao`, `periodoTrocaCategoria`, `proximaCategoria`) VALUES
-(1, 'Lancamento', 'Tem nao', '3', 'Novo'),
-(2, '2', 'Sei la', '3', '3');
+(1, 'Super Lancamento', '', '3', '2'),
+(2, 'Lancamento', '', '5', '3'),
+(3, 'Normal', '', '12', '4'),
+(4, 'Catalogo', '', '', '');
 
 -- --------------------------------------------------------
 
@@ -75,7 +77,10 @@ CREATE TABLE `clientes` (
 --
 
 INSERT INTO `clientes` (`codCliente`, `nome`, `sexo`, `dataDeNasc`, `estadoCIvil`, `cpf`, `rg`, `cep`, `bairro`, `logradouro`, `numero`, `complemento`, `estado`, `cidade`, `telefone1`, `telefone2`, `celular`, `email`, `codDependente`) VALUES
-(1, 'Zeca Urubu', '1', '1', '1', '1', '1', '1', '1', '1', '1', '1', '1', '1', '1', '1', '1', '1', NULL);
+(1, 'Nicolas Marcelo Carvalho', 'M', '25101995', 'Solteiro', '47587421629', '484453439', '36016180', 'Paineiras', 'Praca Coronel Aprigio Ribeiro', '455', '1', 'Minas Gerais', 'Juiz de Fora', '3232558878', '', '32984885778', '', NULL),
+(2, 'Antonella Olivia Fernandes', 'F', '24081992', 'Casada', '69030229616', '157787333', '36020580', 'Vila Ideal', 'Rua Jose Monteiro', '24', '1', 'Minas Gerais', 'Juiz de Fora', '3232256497', '', '32984557889', '', NULL),
+(3, 'Bianca Sophia Campos', 'F', '08041985', 'Casada', '62156123675', '264196132', '36055120', 'Grama', 'Rua Cinco', '45', '2', 'Minas Gerais', 'Juiz de Fora', '3232554685', '', '32985253211', '', NULL),
+(4, 'Stefany Rafaela Carolina Oliveira', 'F', '25021970', 'Casada', '74238242670', '350133347', '36035790', 'Vila Montanhesa', 'Rua Vereador Jose Gasparete', '251', '2', 'Minas Gerais', 'Juiz de Fora', '3232254487', '', '32991995874', '', NULL);
 
 -- --------------------------------------------------------
 
@@ -116,8 +121,16 @@ CREATE TABLE `filmes` (
 --
 
 INSERT INTO `filmes` (`codFilme`, `titulo`, `tituloOriginal`, `nomeDiretor`, `nomeAtorPrincipal`, `nomePremiacoes`, `anoProducao`, `anoLancamento`, `codGenero`, `codFornecedor`, `codCategoria`, `codTipoDeMidia`) VALUES
-(1, '23', '1', '1', '1', '1', '1', '2', 2, 1, 1, 1),
-(2, '2', '2', '2', '2', '2', '2', '2', 1, 1, 1, 1);
+(1, 'Fragmentado', 'Fragmentado', '', '', '', '2017', '2017', 6, 2, 1, 3),
+(2, 'O Chamado 3', 'O Chamado 3', '', '', '', '2016', '2017', 2, 5, 1, 1),
+(3, 'Batman Vs Superman - A origem da Justica', 'Batman Vs Superman - A origem da Justica', '', '', '', '2016', '2016', 1, 1, 2, 1),
+(4, 'Velozes e Furiosos 7', 'Velozes e Furiosos 7', '', '', '', '2016', '2016', 1, 2, 1, 2),
+(5, 'O chamado 2', 'O chamado 2', '', '', '', '', '', 2, 4, 4, 3),
+(6, 'Os Smurfs', 'Os Smurfs', '', '', '', '', '', 7, 3, 4, 3),
+(7, 'Madagascar', 'Madagascar', '', '', '', '2002', '2002', 7, 1, 4, 3),
+(8, 'Mad Max', 'Mad Max', '', '', '', '2016', '2016', 1, 3, 2, 2),
+(9, 'O homem de aco', 'O homem de aco', '', '', '', '2013', '2013', 1, 4, 3, 3),
+(10, 'Logan', 'Logan', '', '', '', '2017', '2017', 1, 1, 1, 1);
 
 -- --------------------------------------------------------
 
@@ -149,9 +162,11 @@ CREATE TABLE `fornecedores` (
 --
 
 INSERT INTO `fornecedores` (`codFornecedor`, `razaoSocial`, `nomeFantasia`, `cnpj`, `nomeContato`, `cep`, `bairro`, `logradouro`, `numero`, `complemento`, `estado`, `cidade`, `telefone1`, `telefone2`, `celular`, `email`) VALUES
-(1, '1', '1', '1', '1', '1', '1', '1', '1', '1', '1', '1', '1', '1', '1', '1'),
-(2, '2', '2', '2', '2', '2', '2', '2', '2', '2', '2', '2', '2', '2', '2', '2'),
-(3, '3', '3', '3', '3', '3', '3', '3', '3', '3', '3', '3', '3', '3', '3', '3');
+(1, 'Felipe e Agatha Videos Ltda', 'Felipe e Agatha Videos Ltda', '06274399000185', 'Felipe', '36046680', 'Quintas das Avenidas', 'Rua Darcilia B. Nobrega', '215', '1', 'Minas Gerais', 'Juiz de Fora', '3237838947', '', '32985432872', 'qualidade@felipeagatha.com.br'),
+(2, 'Benjamin e Nicolas Locadora Ltda', 'Benjamin e Nicolas Locadora Ltda', '36684957000141', 'Nicolas', '36051110', 'Alto Grajau', 'Nossa Senhora da Abadia', '2', '2', 'Minas Gerais', 'Juiz de Fora', '3232255456', '', '32984887858', 'contato@benjaminnicolas.com.br'),
+(3, 'Yuri e Eduarda Midias ME', 'Yuri e Eduarda Midias ME', '78165527000163', 'Yuri', '38701213', 'Copacabana', 'Avenida Carlos Nogueira ', '3251', '3', 'Minas Gerais', 'Patos de Minas', '3435039157', '', '34988545478', ''),
+(4, 'Alice e Marcelo Filmes Ltda', 'Alice e Marcelo Filmes Ltda', '01724445000187', 'Alice', '28910450', 'Jardim Flamboyant', 'Rua AntÃ´nio de Oliveira Gama', '632', '', 'Rio de Janeiro', 'Cabo Frio', '2238803902', '', '22992364808', 'manutencao@alicemarcelo.com.br'),
+(5, 'Brenda e Benjamin Grafica ME', 'Brenda e Benjamin Grafica ME', '36780887000125', 'Brenda', '11676470', 'BalneaÂ¡rio Copacabana', 'Rua Yasushige Nagae', '552', '1', 'SÃ?Â£o Paulo', 'Caraguatatuba', '1236608570', '', '12992670658', 'atendimento@brendabenjamin.com.br');
 
 -- --------------------------------------------------------
 
@@ -171,7 +186,13 @@ CREATE TABLE `generos` (
 
 INSERT INTO `generos` (`codGenero`, `nome`, `descricao`) VALUES
 (1, 'Acao', ''),
-(2, 'Terror', 'Doideras');
+(2, 'Terror', ''),
+(3, 'Comedia', ''),
+(4, 'Romance', ''),
+(5, 'Drama', ''),
+(6, 'Suspense', ''),
+(7, 'Animacao', ''),
+(8, 'Ficcao', '');
 
 -- --------------------------------------------------------
 
@@ -204,8 +225,10 @@ CREATE TABLE `tipodemidia` (
 --
 
 INSERT INTO `tipodemidia` (`codTipoDeMidia`, `nome`, `descricao`) VALUES
-(1, 'BlueRay', 'Midia cara da porra'),
-(2, '2', 'Dvd dad');
+(1, 'BlueRay', ''),
+(2, 'DVD-DL', ''),
+(3, 'DVD', 'Normal'),
+(4, 'CD', '');
 
 -- --------------------------------------------------------
 
@@ -241,7 +264,8 @@ CREATE TABLE `usuarios` (
 --
 
 INSERT INTO `usuarios` (`codUsuario`, `nome`, `sexo`, `dataDeNasc`, `estadoCivil`, `cpf`, `rg`, `login`, `senha`, `cep`, `bairro`, `logradouro`, `numero`, `complemento`, `estado`, `cidade`, `telefone1`, `telefone2`, `celular`, `email`) VALUES
-(1, 'Chuck Norris', '1', '1', '1', '1', '1', '1', '1', '1', '1', '1', '1', '1', '1', '1', '1', '1', '1', '1');
+(1, 'Matheus Rafael Juan Lima', 'M', '02051995', 'Solteiro', '16663648608', '180446642', 'mrlima', '5O4E5iqOLG', '36081782', 'Monte Castelo', 'Rua Rogerio Fernando Scoralick', '254', '1', 'Minas Gerais', 'Juiz de Fora', '3225655268', '', '32999602999', ''),
+(2, 'Ramon de Souza Almeida', 'M', '06031992', 'Solteiro', '10039797694', '16640030', 'rsouza', 'rsa2146', '36052110', 'Sao Tarcisio', 'Rua Augusto Vicente Vieira', '45', '2', 'Minas Gerais', 'Juiz de Fora', '32379037', '', '32984919714', '');
 
 --
 -- Indexes for dumped tables
